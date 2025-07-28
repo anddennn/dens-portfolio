13e38221175d4949da6b6fcf77c918ba69526927
anddennn <116547527+anddennn@users.noreply.github.com>
2025-07-28 15:57:30 -0700

    Created navbar
    created navbar for both mobile and desktop responsiveness and accessibility
    
    citation in the css file

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const openButton = document.getElementById('open-sidebar-button')
+const navbar = document.getElementById('navbar')
+
+const media = window.matchMedia('(max-width: 48rem)')
+
+media.addEventListener('change', (e)=> updateNavbar(e))
+
+function updateNavbar(e){
+    const isMobile = e.matches
+    console.log(isMobile);
+    if(isMobile){
+        navbar.setAttribute('inert', '')
+    } else {
+        // desktop device
+        navbar.removeAttribute('inert')
+    }
+}
+
+function openSideBar() {
+    navbar.classList.add('show')
+    openButton.setAttribute('aria-expanded', 'true');
+}
+
+function closeSideBar() {
+    navbar.classList.remove('show')
+    navbar.setAttribute('inert', '')
+}
+
+const navLinks = document.querySelectorAll('nav a')
+navLinks.forEach(link => {
+    link.addEventListener('click', ()=> {
+        closeSideBar()
+    })
+})
+
+updateNavbar(media)
